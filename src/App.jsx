7996ba9eacd83191ed25248ee3ad799bcb15fcd9
Girish kumar <Girishkumar7077@users.noreply.github.com>
2024-10-07 11:56:28 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Login from './Component/Login/Login';
 import Home from './Component/Home/Home';
-import { signInWithPopup, signOut } from "firebase/auth";
-import { auth, provider } from "./Component/FirebaseConfig";
-import { GithubAuthProvider } from "firebase/auth/web-extension";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Component/FirebaseConfig";
+
 
 function App() {
+  const [user, setUser]= useState(false);
+  useEffect(() => {
+    
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser); 
+      } else {
+        setUser(null); 
+      //  window.location.assign("/login")
+      }
+    });
 
+    return () => unsubscribe();
+  }, [auth]);
   return (
     <>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<><Home/></>}></Route>
+          <Route path='/' element={<> {user ? <Home/>:<Login/>}</>}></Route>
           <Route path='/Login' element={<><Login/></>}></Route>
         </Routes>
       </div>
